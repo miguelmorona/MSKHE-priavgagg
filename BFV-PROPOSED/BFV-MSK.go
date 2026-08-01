212fb671f65cb93f996350220b6fa77271aa18ba
Miguel Morona <100560022+miguelmorona@users.noreply.github.com>
2026-08-01 18:48:01 +0200

    Refactor decryption timing function because bug

--- BFV-PROPOSED/BFV-MSK.go
+++ BFV-PROPOSED/BFV-MSK.go
@@ -755,7 +755,7 @@ func decPhase(aggring *AggRings, partialDec [][]ring.Poly, encShareAgg []ring.Po
 
 	// Initialize a timer to measure the decryption phase duration.
 	elapsedDecCloud = time.Duration(0)
-	elapsedDecCloud += runTimedParty(func() {
+	elapsedDecCloud += runTimed(func() {
 
 		buff := aggring.ringQ.NewPoly()
 
@@ -783,7 +783,7 @@ func decPhase(aggring *AggRings, partialDec [][]ring.Poly, encShareAgg []ring.Po
 
 		}
 
-	}, len(partialDec))
+	})
 
 	// Return the decrypted results.
 	return recShare
