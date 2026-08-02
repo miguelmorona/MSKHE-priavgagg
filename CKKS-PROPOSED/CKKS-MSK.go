9e2a122c53e762ce36dbda7a81f4e7b3233985f4
Miguel Morona <100560022+miguelmorona@users.noreply.github.com>
2026-08-02 19:54:38 +0200

    Refactor decryption timing function for clarity

--- CKKS-PROPOSED/CKKS-MSK.go
+++ CKKS-PROPOSED/CKKS-MSK.go
@@ -781,7 +781,7 @@ func decPhase(aggring *AggRings, partialDec [][]ring.Poly, encShareAgg []ring.Po
 
 	// Initialize a timer to measure the decryption phase duration.
 	elapsedDecCloud = time.Duration(0)
-	elapsedDecCloud += runTimedParty(func() {
+	elapsedDecCloud += runTimed(func() {
 
 		//buff := aggring.ringQ.NewPoly()
 
@@ -806,7 +806,7 @@ func decPhase(aggring *AggRings, partialDec [][]ring.Poly, encShareAgg []ring.Po
 
 		}
 
-	}, len(P))
+	})
 
 	// Return the decrypted results.
 	return recShare
